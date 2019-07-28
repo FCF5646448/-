e17e09cf8547bd313e21db7854f5fa1e0f073a81
FCF5646448 <2998000457@qq.com>
2019-07-29 01:36:36 +0800

    slidemune

--- CutShadow/CutShadow/Base/业务/HomeVC.swift
+++ CutShadow/CutShadow/Base/业务/HomeVC.swift
@@ -7,24 +7,96 @@
 //
 
 import UIKit
+import SideMenu
 
 class HomeVC: FCFBaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // 创建一个按钮（点击后显示侧栏菜单）
+        let button = UIButton(type:.system)
+        button.frame = CGRect(x:40, y:100, width:150, height:30)
+        button.setTitle("menu", for:.normal)
+        button.addTarget(self, action:#selector(tapped), for:.touchUpInside)
+        let itemBar = UIBarButtonItem(customView: button)
+        self.navigationItem.leftBarButtonItem = itemBar
+//
+        // 定义一个侧栏菜单
+        let menu = UISideMenuNavigationController(rootViewController: MenuViewController())
+        menu.isNavigationBarHidden = false //侧栏菜单不显示导航栏
+        menu.menuWidth = round(min(WIDTH, HEIGHT) * 0.5)
+        // 将其作为默认的右侧菜单
+        SideMenuManager.default.leftMenuNavigationController = menu
+        
+        // 开启通过边缘滑动打开侧栏菜单的功能
+        SideMenuManager.default.addPanGestureToPresent(toView:
+            self.navigationController!.navigationBar)
+        SideMenuManager.default.addScreenEdgePanGesturesToPresent(toView:
+            self.navigationController!.view)
+        
+        // 将阴影透明度设为0
+        menu.presentationStyle.onTopShadowOpacity = 0
+        
+        
+        // 默认情况下，展开动画播放时间为 0.35 秒
+        menu.presentDuration = 0.35
+        // 默认情况下，消失动画播放时间未 0.35秒
+        menu.dismissDuration = 0.35
+        // 菜单展开时的弹性动画效果
+        menu.animationOptions = .curveEaseOut
+        
+        //动画弹性阻尼和速度
+        menu.usingSpringWithDamping = 1
+        
+        // 默认情况下，做完手势动作后，剩余部分的动画时间为 0.35秒
+        menu.completeGestureDuration = 0.35
+        // 手势动作完成后，剩余部分的动画效果
+        menu.completionCurve = .easeIn
+        
+//        // 阻止状态栏背景变黑
+//        SideMenuManager.default.menuFadeStatusBar = false
+//        // 将侧栏菜单初始fade值设为0.5
+//        SideMenuManager.default.menuAnimationFadeStrength = 0.5
+//
+//        // 将侧栏菜单初始时尺寸为正常值的一半
+//        SideMenuManager.default.menuAnimationTransformScaleFactor = 0.5
+//
+//        // 将侧栏菜单初始时尺寸为正常值的一半
+//        SideMenuManager.default.menuAnimationTransformScaleFactor = 0.5
+//        // 动画过程的背景色
+//        SideMenuManager.default.menuAnimationBackgroundColor = .orange
+        
+    }
+    
+    // 按钮点击响应
+    @objc func tapped(){
+        // 显示侧栏菜单
+        self.present(SideMenuManager.default.leftMenuNavigationController!, animated: true,
+                     completion: nil)
     }
 
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension HomeVC: UISideMenuNavigationControllerDelegate {
+    
+    // 侧栏菜单将要显示时触发
+    func sideMenuWillAppear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("菜单将要显示! (是否有动画: \(animated))")
     }
-    */
-
+    
+    // 侧栏菜单显示完毕时触发
+    func sideMenuDidAppear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("菜单显示完成! (是否有动画: \(animated))")
+    }
+    
+    // 侧栏菜单将要隐藏时触发
+    func sideMenuWillDisappear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("菜单将要隐藏!(是否有动画: \(animated))")
+    }
+    
+    // 侧栏菜单隐藏完毕时触发
+    func sideMenuDidDisappear(menu: UISideMenuNavigationController, animated: Bool) {
+        print("菜单隐藏完毕!(是否有动画: \(animated))")
+    }
 }
---
+++ CutShadow/CutShadow/Base/业务/menu/MenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MenuViewController.swift
+//  CutShadow
+//
+//  Created by 冯才凡 on 2019/7/29.
+//  Copyright © 2019 冯才凡. All rights reserved.
+//
+
+import UIKit
+
+// 菜单视图控制器
+class MenuViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //创建一个重用的单元格
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+    }
+    
+    // 分区数
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // 单元格数量
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    // 返回单元格内容
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath)
+        -> UITableViewCell {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+            cell.textLabel?.text = "菜单\(indexPath.row + 1)"
+            return cell
+    }
+}
